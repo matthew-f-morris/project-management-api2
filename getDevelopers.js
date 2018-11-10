0517327680945e7624813eee62186718ca93f666
Matt Morris <36605894+matthew-f-morris@users.noreply.github.com>
2018-11-10 23:08:07 +0000

    Further Additions to API

---
+++ getDevelopers.js
@@ -0,0 +1,40 @@
+import AWS from "aws-sdk"
+
+export async function main(event, context) {
+
+    const params = {
+        "UserPoolId": "eu-west-2_kKROnK0bk",
+        "AttributesToGet": [ "email" ] 
+    };
+
+    var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
+
+    try {
+        const result = await cognitoidentityserviceprovider.listUsers(params).promise()
+        return success({data: result}); 
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false, error: e, event });
+    }
+}
+
+function buildResponse(statusCode, body){
+    return {
+        statusCode,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify(body)
+    };
+}
+
+export function success(body){
+    return buildResponse(200, body);
+}
+
+export function failure(body){
+    return buildResponse(500, body);
+}
+
+//need to make this specific for developers
