e92f610fa38e2ea552da61812beca0d9cdd559b9
Matt Morris <36605894+matthew-f-morris@users.noreply.github.com>
2018-11-11 19:16:29 +0000

    UpdateProject API

--- updateProject.js
+++ updateProject.js
@@ -3,15 +3,19 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
 
-    const data = (event.body)
+    const data = JSON.parse(event.body)
     const params = {
     TableName: "project_data",    
     Key: {
       project_id: data.project_id,
     },
-    UpdateExpression: "SET projectstatus = :status",
+    UpdateExpression: "SET projectstatus = :status, projectname = :name, projectmanager = :manager, projectdescription = :description, developers = :devs",
     ExpressionAttributeValues: {
       ":status": data.projectstatus,
+      ":name": data.projectname,
+      ":manager": data.projectmanager,
+      ":description": data.projectdescription,
+      ":devs": data.developers
     },
     ReturnValues:"ALL_NEW"
   };    
@@ -21,6 +25,6 @@ export async function main(event, context) {
     return success({ status: true});     
   } catch (e) {
     console.log(e);
-    return failure({ status: false });
+    return failure({ status: false, error: e });
   }
 }
